9cfa7e923b6c879ffdb7decdc02df60206f805b7
Matias Simone <matiassimone.d@gmail.com>
2018-10-25 14:25:01 -0300

    Design Patterns and OOP in JavaScript Ex-3

--- 02-OOP-inheritance/js/scripts.js
+++ 02-OOP-inheritance/js/scripts.js
@@ -10,15 +10,15 @@ class EventEmitter {
             this.events[eventName] = [];
         }
 
-        this.events[eventName].push(callback);
+        this.events[eventName].push(callback);    
     }
 
-    emit(eventName) {
+    emit(eventName, data) {
         const event = this.events[eventName];
 
         if (event) {
             event.forEach(callback => {
-                callback.call(this);
+                callback.call(null, data);
             });
         }
     }
@@ -36,36 +36,57 @@ class Movie extends EventEmitter {
         this.title = name;
         this.year = year;
         this.duration = duration;
+        this.movieCast = [];
 
-        this.pSelector = document.querySelector('p');
+        const logger = new Logger();
 
-        super.on("play-movie", function playMovie() {
-            this.logActions(this.title + " is beginning !, its duration is " + this.duration);
-        })
+        super.on("play-movie", data => {
+            logger.log(data);
+        });
 
-        super.on("pause-movie", function pauseMovie() {
-            this.logActions(this.title + " is paused..");
-        })
+        super.on("pause-movie", data => {
+            logger.log(data);
+        });
 
-        super.on("resume-movie", function resumeMovie() {
-            this.logActions(this.title + " is resumed..");
-        })
+        super.on("resume-movie", data => {
+            logger.log(data);
+        });
+
+        super.on("cast-movie", data => {
+            logger.log(data);
+        });
     }
 
     play() {
-        super.emit("play-movie");
+        super.emit("play-movie", (this.title + " is beginning !, its duration is " + this.duration + " min..") );
     }
 
     pause() {
-        super.emit("pause-movie");
+        super.emit("pause-movie", (this.title + " is paused..") );
     }
 
     resume() {
-        super.emit("resume-movie");
+        super.emit("resume-movie", (this.title + " is resumed..") );
     }
 
-    logActions(actionToLog) {
-        this.pSelector.innerHTML = actionToLog;
+    addCast(cast) {
+        
+        if (Array.isArray(cast)) {
+            this.movieCast = this.movieCast.concat(cast);
+
+        } else {
+            this.movieCast.push(cast);
+        }
+    }
+
+    showCast(){
+        
+        let actors = "";
+
+        this.movieCast.forEach((actor) => {
+            actors = actors + " - " + actor.name + " - ";
+        })
+        super.emit("cast-movie", (actors) );  
     }
 }
 
@@ -75,8 +96,33 @@ class Actor {
         this.name = name;
         this.age = age;
     }
-};
+}
+
+class Logger extends EventEmitter {
+
+    constructor() {
+        super();
+        this.pSelector = document.querySelector('p');
+    }
+
+    listen(eventName, callback) {
+        super.on(eventName, callback);
+    }
+
+    log(info) {
+        this.pSelector.innerHTML = info;
+    }
+}
 
 
-let movie = new Movie('Lord of the Ring', 2005, "2 Hs");
-let actor = new Actor('Pepe', 27);
+const terminator = new Movie('Terminator I', 1985, 60);
+
+const arnold = new Actor('Arnold Schwarzenegger', 50);
+const actors = [
+    new Actor('Paul Winfield', 50),
+    new Actor('Michael Biehn', 50),
+    new Actor('Linda Hamilton', 50)
+];
+
+terminator.addCast(arnold);
+terminator.addCast(actors);
